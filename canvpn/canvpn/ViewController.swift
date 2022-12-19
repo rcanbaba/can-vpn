6cdc64f4b699728b980ac7e351041a81ddc0ab9c
rcanbaba <rcanbabaoglu@gmail.com>
2022-12-19 21:27:14 +0300

    project created.

---
+++ canvpn/canvpn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  canvpn
+//
+//  Created by Can Babaoğlu on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
