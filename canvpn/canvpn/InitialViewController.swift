81b57ead040cc2048ed335aa3929c8b490b3bccd
rcanbaba <rcanbabaoglu@gmail.com>
2022-12-19 21:36:02 +0300

    snapkit added.

--- canvpn/canvpn/InitialViewController.swift
+++ canvpn/canvpn/InitialViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class InitialViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.blue
     }
-
 
 }
 
